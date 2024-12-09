f4ecc07049a305c67037701ce49a94f1ac215e2a
Vikhyath-Shetty <vikhyath.v.shetty@gmail.com>
2024-12-10 02:11:17 +0530

    final prototype

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const asyncHandler = require("express-async-handler");
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+//fetching respose from api
+const getResponse = asyncHandler(async (req, res) => {
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const { prompt } = req.body;
+  const result = await model.generateContent(prompt);
+  if (!result) {
+    res.status(500).json({ message: "Sorry,Try again!" });
+  }
+  const message = result.response.text();
+  res.status(200).json({ message: message });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
+
+app.use(cors());
+app.use(express.json());
+app.post("/api/prompt", getResponse);
---
+++ frontend/script.js
@@ -0,0 +1,75 @@
+const messageInput = document.getElementById("message-input");
+const sendBtn = document.getElementById("send-btn");
+const chatMessages = document.getElementById("chat-messages");
+
+renderHistory();
+
+sendBtn.addEventListener("click", startChat);
+messageInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") startChat();
+});
+
+function renderPrompt(prompt) {
+  const userMessage = document.createElement("div");
+  userMessage.classList.add("message", "user-message");
+  userMessage.textContent = prompt;
+  chatMessages.appendChild(userMessage);
+}
+
+function renderResponse(message) {
+  const botMessageEl = document.createElement("div");
+  botMessageEl.classList.add("message", "bot-message");
+  botMessageEl.textContent = message;
+  chatMessages.appendChild(botMessageEl);
+}
+
+function saveToStorage(prompt, message) {
+  const history = sessionStorage.getItem("chatHistory");
+  const array = history ? JSON.parse(history) : [];
+  array.push({ prompt, message });
+  sessionStorage.setItem("chatHistory", JSON.stringify(array));
+}
+
+function renderHistory() {
+  const history = sessionStorage.getItem("chatHistory");
+  if (!history) return;
+  const historyArr = JSON.parse(history);
+  historyArr.forEach(({ prompt, message }) => {
+    renderPrompt(prompt);
+    renderResponse(message);
+  });
+}
+
+
+function sendPrompt(prompt) {
+  return fetch("http://localhost:5001/api/prompt", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ prompt }),
+  })
+    .then((response) => response.json())
+    .then((data) => data.message);
+}
+
+async function startChat() {
+  const prompt = messageInput.value.trim();
+  if (prompt === "") return;
+
+  // rendering the user message
+  renderPrompt(prompt);
+
+  // sending the request to backend
+  const message = await sendPrompt(prompt);
+
+  // rendering the api response
+  renderResponse(message);
+
+  // Clear input and scroll to bottom
+  messageInput.value = "";
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+
+  //save to storage
+  saveToStorage(prompt, message);
+}
